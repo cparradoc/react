9bfd1deebf6b35c3abc4a665a45a6e8ee152eb3e
cparradoc <cparradoc@minsait.com>
2022-03-02 16:08:40 +0100

    function that changes player's turn

--- game_hub/src/components/Tictactoe/Tictactoe.js
+++ game_hub/src/components/Tictactoe/Tictactoe.js
@@ -10,6 +10,8 @@ export const Tictactoe = () => {
     const [isGameFinished, setGameFinished] = useState(false);
     //tablero de celdas en el que se jugará
     const [cellBoard, setCellBoard] = useState([]);
+    //propiedad para determinar de quién es el turno, 0 turno de O y 1 turno de X
+    const [turnOX, setTurnOX] = useState(0);
 
     const winCase = [ 
       [0, 1, 2], [3, 4, 5], [6, 7, 8],
@@ -26,15 +28,29 @@ export const Tictactoe = () => {
     }, [isStarted]);
 
 
+    function changePlayer() {
+      if(turnOX) {
+        setTurnOX(0);
+      }else {
+        setTurnOX(1);
+      }
+    }
+
     function restartGame() {
       setGameFinished(false);
       setIsStarted(1);
+
     }
 
     return (
       <div>
         <h1>Tictactoe</h1>
         <button className="start" onClick={restartGame}>Start Game</button>
+        {turnOX ? (
+          <h2>Es el turno de X</h2>
+        ) : (
+          <h2>Es el turno de O</h2>
+        )}
       </div>
     );
   };

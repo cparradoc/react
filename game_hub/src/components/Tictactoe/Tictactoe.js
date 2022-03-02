1258e365d761aeaf1c113a5c991ba473508cf2ef
cparradoc <cparradoc@minsait.com>
2022-03-02 13:41:31 +0100

    boton comenzar partida

--- game_hub/src/components/Tictactoe/Tictactoe.js
+++ game_hub/src/components/Tictactoe/Tictactoe.js
@@ -1,10 +1,27 @@
 import React from 'react';
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import './Tictactoe.css'
 
 export const Tictactoe = () => {
+
+    //propiedad del estado de tipo boolean para comenzar/resetear partida
+    const [isStarted, setIsStarted] = useState(false);
+
+    useEffect(() => {
+        if(isStarted) {
+            console.log("Partida comenzada!");
+        } else {
+            console.log("Partida reiniciada!");
+        }
+    }, [isStarted]);
+
+
+
+
     return (
       <div>
         <h1>Tictactoe</h1>
+        <button className="start" onClick={() => setIsStarted(!isStarted)}>Start Game</button>
       </div>
     );
   };

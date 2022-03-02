23a5820eac08caa935a44d0d7e13a477058c0fc1
cparradoc <cparradoc@minsait.com>
2022-03-02 11:13:25 +0100

    basic app.js with home component

--- hub_juegos/src/App.js
+++ hub_juegos/src/App.js
@@ -1,57 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Home } from './components/Home';
 import { Counter } from './components/Counter';
 import './App.css';
+import { Router } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <Router>
+      <div className="App">
+        <header>
         <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+        </header>
+
+        <Switch>
+          <Route path="/counter" extract component={Counter} />
+          <Route path="/" extract component={Home} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 

469e2baa32820889d2beaf923213114d6ffc8a87
cparradoc <cparradoc@minsait.com>
2022-03-02 12:13:53 +0100

    clear project

--- hub_juegos/src/app/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
-
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-    //user: userReducer, si se quisiese soporte para datos de un usuario
-  },
-});

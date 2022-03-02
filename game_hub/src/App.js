b8c52bb1ae7d0130bc18dc38443c64d720dabd03
cparradoc <cparradoc@minsait.com>
2022-03-02 13:29:02 +0100

    react logo header removed and height fixed

--- game_hub/src/App.js
+++ game_hub/src/App.js
@@ -13,7 +13,6 @@ function App() {
     <Router>
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
         </header>
 
         <Routes>

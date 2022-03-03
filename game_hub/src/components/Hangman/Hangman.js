0526f6fe9fce541069f42282262a5249699d3fea
cparradoc <cparradoc@minsait.com>
2022-03-03 11:05:03 +0100

    hangman variables defined

--- game_hub/src/components/Hangman/Hangman.js
+++ game_hub/src/components/Hangman/Hangman.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useEffect } from "react";
 
 export const Hangman = () => {
+
+  const [word, setWord] = useState(""); //palabra
+  const [letter, setLetter] = useState(""); //letra
+  const [hint, setHint] = (""); //pista
+  const numberOfTries = 6;
+  const [nTry, setNTry] = useState(0);
+
+
     return (
       <div>
         <h1>Hangman</h1>

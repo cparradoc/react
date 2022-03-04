7d46468af692ffa7dfcd68514bf485dde62833d4
cparradoc <cparradoc@minsait.com>
2022-03-04 08:47:41 +0100

    basic structure

--- game_hub/src/components/Sudoku/Sudoku.js
+++ game_hub/src/components/Sudoku/Sudoku.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useEffect } from "react";
+import { makepuzzle, solvepuzzle } from "sudoku";
 
 export const Sudoku = () => {
+
+  const [cellBoard, setCellBoard] = useTtate(null);
+
+  useEffect (() => {
+
+  });
+
+  const handleFormSubmit = e => {
+
+  };
+
+
     return (
       <div>
       <Link to="/">Go back to main game hub menu</Link>
         <h1>Sudoku</h1>
+        <form onSubmit={handleFormSubmit}>
+
+        </form>
       </div>
     );
   };

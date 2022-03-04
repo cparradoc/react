b71f097da85e9609e98c88abcf0350f4a892373f
cparradoc <cparradoc@minsait.com>
2022-03-04 12:19:08 +0100

    set sudoku solution

--- game_hub/src/components/Sudoku/Sudoku.js
+++ game_hub/src/components/Sudoku/Sudoku.js
@@ -6,23 +6,29 @@ import { makepuzzle, solvepuzzle } from "sudoku";
 export const Sudoku = () => {
 
   const [cellBoard, setCellBoard] = useState(null);
+  const [sudokuDone, setSudokuDone] = useState(false);
 
   useEffect (() => {
-
+    const newBoard = makepuzzle();
+    setSudokuDone(solvepuzzle(newBoard));
   });
 
   const handleFormSubmit = e => {
 
   };
 
+  const CreateBoard = () => {
 
-    return (
-      <div>
-      <Link to="/">Go back to main game hub menu</Link>
-        <h1>Sudoku</h1>
-        <form onSubmit={handleFormSubmit}>
+  }
 
-        </form>
-      </div>
-    );
-  };
+
+  return (
+    <div>
+    <Link to="/">Go back to main game hub menu</Link>
+      <h1>Sudoku</h1>
+      <form onSubmit={handleFormSubmit}>
+
+      </form>
+    </div>
+  );
+};

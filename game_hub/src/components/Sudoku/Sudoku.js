2206457e64cfb8cf89151c01f1ff188b5e2ac522
cparradoc <cparradoc@minsait.com>
2022-03-04 14:33:37 +0100

    create board function to print a sudoku (still empty)

--- game_hub/src/components/Sudoku/Sudoku.js
+++ game_hub/src/components/Sudoku/Sudoku.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { useEffect, useState } from "react";
 import { makepuzzle, solvepuzzle } from "sudoku";
+import Grid from '@material-ui/core/Grid';
 
 export const Sudoku = () => {
 
   const [cellBoard, setCellBoard] = useState(null);
   const [sudokuDone, setSudokuDone] = useState(false);
+  const [renderSudoku, setRenderSudoku] = useState(0);
 
   useEffect (() => {
     const newBoard = makepuzzle();
@@ -20,14 +22,21 @@ export const Sudoku = () => {
 
     setCellBoard(newBoard);
 
-  });
+  }, [renderSudoku]);
 
   const handleFormSubmit = e => {
 
   };
 
   const createBoard = () => {
-    
+    let board = [];
+    for (let i = 0; i < cellBoard.length; i++) {
+
+    }
+
+    return <Grid>{board}</Grid>
+
+
   };
 
   const restartGame = () => {
@@ -41,7 +50,7 @@ export const Sudoku = () => {
       <h1>Sudoku</h1>
         <button onClick={restartGame}>Restart game</button>
       <form onSubmit={handleFormSubmit}>
-
+        <createBoard></createBoard>
       </form>
     </div>
   );
